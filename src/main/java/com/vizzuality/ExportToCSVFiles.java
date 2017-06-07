dd1260c88bbd5db1fcc25849a1dff4efd6ab3123
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-06-07 10:19:04 +0200

    Add new program to export TSV to several CSV files that are easier to be imported from Cypher

---
+++ src/main/java/com/vizzuality/ExportToCSVFiles.java
@@ -0,0 +1,57 @@
+package com.vizzuality;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by pabloparejatobes on 6/7/17.
+ */
+public class ExportToCSVFiles {
+
+    public static void main(String[] args) throws Exception{
+        if(args.length != 1){
+            System.out.println("This program expect one parameter: TSV file name");
+        }else{
+
+            BufferedReader reader = new BufferedReader(new FileReader(new File(args[0])));
+            String line;
+            String[] columns = reader.readLine().split("\t");
+
+            ArrayList<String> relTypes = new ArrayList<>();
+            HashMap<String, BufferedWriter> writersMap = new HashMap<>();
+
+            for(int i=2;i<columns.length;i++){
+                String relName = columns[i];
+                relTypes.add(relName);
+                writersMap.put(relName, new BufferedWriter(new FileWriter(new File("csv_files/" + relName + ".csv"))));
+            }
+
+            BufferedWriter nodesFileWriter = new BufferedWriter(new FileWriter(new File("csv_files/nodes.csv")));
+
+            while((line = reader.readLine()) != null){
+                String[] values = line.split("\t");
+                String valueID = values[0];
+                nodesFileWriter.write( valueID + "," + values[1] + "\n");
+                for(int i=2;i<values.length;i++){
+                    String relValue = values[i];
+                    if(relValue.trim().length() > 0){
+                        String[] relColumns = relValue.split(",");
+                        for(String relColumn : relColumns){
+                            writersMap.get(relTypes.get(i-2)).write(valueID + "," + relColumn + "\n");
+                        }
+                    }
+                }
+            }
+            reader.close();
+
+            System.out.println("Closing files...");
+            nodesFileWriter.close();
+            for(String key: writersMap.keySet()){
+                writersMap.get(key).close();
+            }
+            System.out.println("Done!");
+
+        }
+    }
+}
