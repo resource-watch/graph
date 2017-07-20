1c1654238b9478e13a006a137e6708d2a03090f9
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-07-20 12:42:39 +0200

    Add last version of files

--- src/main/java/com/vizzuality/ExportToCSVFiles.java
+++ src/main/java/com/vizzuality/ExportToCSVFiles.java
@@ -21,7 +21,7 @@ public class ExportToCSVFiles {
             ArrayList<String> relTypes = new ArrayList<>();
             HashMap<String, BufferedWriter> writersMap = new HashMap<>();
 
-            for(int i=2;i<columns.length;i++){
+            for(int i=3;i<columns.length;i++){
                 String relName = columns[i];
                 relTypes.add(relName);
                 writersMap.put(relName, new BufferedWriter(new FileWriter(new File("csv_files/" + relName + ".csv"))));
@@ -29,16 +29,20 @@ public class ExportToCSVFiles {
 
             BufferedWriter nodesFileWriter = new BufferedWriter(new FileWriter(new File("csv_files/nodes.csv")));
 
+            // Split definition line
+            reader.readLine();
+
             while((line = reader.readLine()) != null){
                 String[] values = line.split("\t");
                 String valueID = values[0];
+//                System.out.println(values[0] + "," + values[1]);
                 nodesFileWriter.write( valueID.trim() + "," + values[1].trim() + "\n");
-                for(int i=2;i<values.length;i++){
+                for(int i=3;i<values.length;i++){
                     String relValue = values[i].trim();
                     if(relValue.length() > 0){
                         String[] relColumns = relValue.split(",");
                         for(String relColumn : relColumns){
-                            writersMap.get(relTypes.get(i-2)).write(valueID + "," + relColumn.trim() + "\n");
+                            writersMap.get(relTypes.get(i-3)).write(valueID + "," + relColumn.trim() + "\n");
                         }
                     }
                 }
