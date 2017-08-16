fcd1b21bcda110602636acab3225135b1ce88afd
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-08-16 09:11:14 +0200

    Update code that exports the spreadsheet to several CSV files so that it takes into account the the new column that holds the index of the term (first column). CSV files have been updated with the latest version of the spreadsheet.

--- src/main/java/com/vizzuality/ExportToCSVFiles.java
+++ src/main/java/com/vizzuality/ExportToCSVFiles.java
@@ -21,7 +21,7 @@ public class ExportToCSVFiles {
             ArrayList<String> relTypes = new ArrayList<>();
             HashMap<String, BufferedWriter> writersMap = new HashMap<>();
 
-            for(int i=3;i<columns.length;i++){
+            for(int i=4;i<columns.length;i++){
                 String relName = columns[i];
                 relTypes.add(relName);
                 writersMap.put(relName, new BufferedWriter(new FileWriter(new File("csv_files/" + relName + ".csv"))));
@@ -34,18 +34,18 @@ public class ExportToCSVFiles {
 
             while((line = reader.readLine()) != null){
                 String[] values = line.split("\t");
-                String valueID = values[0];
+                String valueID = values[1];
                 String synonymsSt = "";
-                if (values.length > 2) {
-                    synonymsSt = values[2].trim();
+                if (values.length > 3) {
+                    synonymsSt = values[3].trim();
                 }
-                nodesFileWriter.write( valueID.trim() + "," + values[1].trim() + "," + synonymsSt + "\n");
-                for(int i=3;i<values.length;i++){
+                nodesFileWriter.write( valueID.trim() + "," + values[2].trim() + "," + synonymsSt + "\n");
+                for(int i=4;i<values.length;i++){
                     String relValue = values[i].trim();
                     if(relValue.length() > 0){
                         String[] relColumns = relValue.split(",");
                         for(String relColumn : relColumns){
-                            writersMap.get(relTypes.get(i-3)).write(valueID + "," + relColumn.trim() + "\n");
+                            writersMap.get(relTypes.get(i-4)).write(valueID + "," + relColumn.trim() + "\n");
                         }
                     }
                 }
